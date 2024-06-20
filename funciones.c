a8e0eab5bbe934e673d95b34a556c41f6f99c293
Martin Msq <martinmsq625@gmail.com>
2024-06-19 21:04:33 -0500

    first commit

---
+++ funciones.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include "funciones.h"
+#include <string.h>
+
+void buscarPorTamano(int *numHabitacion, char habitaciones[][3][40], double precios[]) {
+    char tipo[40];
+    printf("Ingrese el tamaño de habitacion que desea buscar: ");
+    scanf("%s", tipo);
+    printf("#\t\tTamaño\t\tHotel\t\tPrecio\n");
+    for (int i = 0; i < 9; i++) {
+        if (strcmp(habitaciones[i][1], tipo) == 0) {
+            printf("%s\t\t%s\t\t%s\t\t%.2lf\n", habitaciones[i][0], habitaciones[i][1], habitaciones[i][2], precios[i]);
+        }
+    }
+    printf("Elija la habitacion que desea reservar: ");
+    scanf("%d", numHabitacion);
+}
+
+void buscarHotel(int *numHabitacion, char habitaciones[][3][40], double precios[]) {
+    char nombreHotel[40];
+    printf("Ingrese el nombre del hotel que desea buscar: ");
+    scanf("%s", nombreHotel);
+    printf("#\t\tTamaño\t\tHotel\t\tPrecio\n");
+    for (int i = 0; i < 9; i++) {
+        if (strcmp(habitaciones[i][2], nombreHotel) == 0) {
+            printf("%s\t\t%s\t\t%s\t\t%.2lf\n", habitaciones[i][0], habitaciones[i][1], habitaciones[i][2], precios[i]);
+        }
+    }
+    printf("Elija la habitacion que desea reservar: ");
+    scanf("%d", numHabitacion);
+}
+
+void realizarReserva(int numHabitacion, char habitaciones[][3][40], char clientes[][2][40], int reservas[][4]) {
+    int clienteNuevo = -1;
+    for (int i = 0; i < 5; i++) {
+        if (clientes[i][0][0] == '\0') {
+            clienteNuevo = i;
+            break;
+        }
+    }
+    if (clienteNuevo == -1) {
+        printf("No hay espacio para mas clientes.\n");
+        return;
+    }
+
+    if (reservas[numHabitacion - 1][1] != -1) {
+        printf("La habitacion %d ya esta reservada.\n", numHabitacion);
+        return;
+    }
+
+    printf("Ingrese el nombre del cliente: ");
+    scanf("%s", clientes[clienteNuevo][0]);
+    printf("Ingrese la cédula del cliente: ");
+    scanf("%s", clientes[clienteNuevo][1]);
+
+    for (int i = 0; i < 10; i++) {
+        if (reservas[i][0] == -1) {
+            reservas[i][0] = clienteNuevo;
+            reservas[i][1] = numHabitacion - 1;
+            reservas[i][2] = 0;
+            reservas[i][3] = 0;
+            printf("Reserva realizada correctamente.\n");
+            return;
+        }
+    }
+
+    printf("No hay espacio para más reservas.\n");
+}
+
+void buscarReservaPorCedula(int *numReserva, char clientes[][2][40], int reservas[][4]) {
+    char cedula[40];
+    printf("Ingrese la cedula que desea buscar la reserva: ");
+    scanf("%s", cedula);
+    for (int i = 0; i < 10; i++) {
+        if ((reservas[i][0] != -1) && (strcmp(clientes[reservas[i][0]][1], cedula) == 0)) {
+            *numReserva = i;
+            return;
+        }
+    }
+    *numReserva = -1;
+}
+
+void imprimirReserva(int numReserva, int reservas[][4], char habitaciones[][3][40], double precios[], char clientes[][2][40]) {
+    if (numReserva != -1) {
+        int clienteIndex = reservas[numReserva][0];
+        int habitacionIndex = reservas[numReserva][1];
+        char *nombreCliente = clientes[clienteIndex][0];
+        printf("Reserva %d: Cliente %s en Habitación %s (%s) de %s con precio %.2lf\n",
+               numReserva+1, nombreCliente, habitaciones[habitacionIndex][0], habitaciones[habitacionIndex][1],
+               habitaciones[habitacionIndex][2], precios[habitacionIndex]);
+        if (reservas[numReserva][3] == 1) {
+            printf("Estado: Pagada\n");
+        } else {
+            printf("Estado: No Pagada\n");
+        }
+    } else {
+        printf("Reserva no encontrada.\n");
+    }
+}
+
+void pagarReserva(int numReserva, int reservas[][4], char habitaciones[][3][40], double precios[]) {
+    if (numReserva != -1) {
+        if (reservas[numReserva][3] == 1) {
+            printf("La reserva ya está pagada.\n");
+        } else {
+            reservas[numReserva][3] = 1; 
+            printf("Reserva %d pagada.\n", numReserva+1);
+        }
+    } else {
+        printf("Reserva no encontrada.\n");
+    }
+}
---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include "funciones.h"
+
+int main() {
+    char clientes[5][2][40] = {
+        {"Juan", "1789654123"},
+        {"", ""},
+        {"", ""},
+        {"", ""},
+        {"", ""}
+    };
+
+    char habitaciones[9][3][40] = {
+        {"1", "simple", "Dan_Carlton"},
+        {"2", "doble", "Dan_Carlton"},
+        {"3", "triple", "Dan_Carlton"},
+        {"4", "simple", "Swissotel"},
+        {"5", "doble", "Swissotel"},
+        {"6", "triple", "Swissotel"},
+        {"7", "simple", "Sheraton"},
+        {"8", "doble", "Sheraton"},
+        {"9", "triple", "Sheraton"}
+    };
+
+    double precios[9] = {90, 120, 160, 65, 100, 140, 85, 110, 150};
+
+    int reservas[10][4] = {
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0},
+        {-1, -1, -1, 0}
+    };
+
+    int opcion, numHabitacion, numReserva;
+    do {
+        printf("\nEscoja una opcion:\n1. Buscar Habitacion\n2. Realizar reserva\n3. Ver reservas\n4. Pagar Reserva\n>> ");
+        scanf("%d", &opcion);
+        switch (opcion) {
+            case 1:
+                printf("1. Por tamaño\n2. Por Hotel\n>> ");
+                scanf("%d", &opcion);
+                switch (opcion) {
+                    case 1:
+                        buscarPorTamano(&numHabitacion, habitaciones, precios);
+                        break;
+                    case 2:
+                        buscarHotel(&numHabitacion, habitaciones, precios);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 2:
+                realizarReserva(numHabitacion, habitaciones, clientes, reservas);
+                break;
+            case 3:
+                buscarReservaPorCedula(&numReserva, clientes, reservas);
+                imprimirReserva(numReserva, reservas, habitaciones, precios, clientes);
+                break;
+            case 4:
+                buscarReservaPorCedula(&numReserva, clientes, reservas);
+                pagarReserva(numReserva,reservas,habitaciones,precios);
+                break;
+            default:
+                break;
+
+        }
+        printf("Desea elegir otra opcion\n1.Si\n2.No\n>>");
+        scanf("%d", &opcion);
+    } while (opcion == 1);
+
+    return 0;
+}
